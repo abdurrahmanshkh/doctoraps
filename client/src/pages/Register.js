5690eeea99b2225ca3851d674e6390125f9cc241
Abdur Rehman Shaikh <abdurrahman19702@gmail.com>
2024-07-28 11:58:38 +0530

    Created React App

---
+++ client/src/pages/Register.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Form } from 'antd';
+
+const Register = () => {
+  return (
+    <>
+      <div className='form-container'>
+        <Form layout="vertical">
+          
+
+        </Form>
+
+      </div>
+    </>
+  )
+}
+
+export default Register
